fbadc44441d3fd90c5e340e89d2dad3a17bfba0c
post1blues <yura1kovalyov@gmail.com>
2021-08-28 00:51:48 +0300

    Customize PlaceAdmin with ImageInline

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from places.models import Place, Image, Location
 
 
-admin.site.register(Place)
 admin.site.register(Image)
 admin.site.register(Location)
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
