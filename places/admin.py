05fb357aca17aff5a0fbd16a73e355f164825e43
post1blues <yura1kovalyov@gmail.com>
2021-08-28 01:26:19 +0300

    Add image preview in PlaceAdmin

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from places.models import Place, Image, Location
 
 
@@ -8,8 +10,14 @@ admin.site.register(Location)
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ("image", "image_preview", "serial_number")
+    readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        return format_html("<img src=\"{}\" style=\"max-height: 200px\" />".format(obj.image.url))
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
+
