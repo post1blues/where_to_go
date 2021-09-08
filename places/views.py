ef416f4a76d0121b16509509d754dec3ad66f6cc
post1blues <yura1kovalyov@gmail.com>
2021-09-08 16:41:00 +0300

    Fix place_view with new names of Place model

--- places/views.py
+++ places/views.py
@@ -34,8 +34,8 @@ def place_view(request, pk):
     context = {
         "title": place.title,
         "imgs": [img.image.url for img in place.imgs.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
+        "description_short": place.short_description,
+        "description_long": place.long_description,
         "coordinates": {
             "lng": place.lng,
             "lat": place.lat
