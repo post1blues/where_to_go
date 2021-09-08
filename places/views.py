9c5248774a18fe24c71d3467761145176a3fac88
post1blues <yura1kovalyov@gmail.com>
2021-09-08 17:36:34 +0300

    Remove if statement

--- places/views.py
+++ places/views.py
@@ -7,21 +7,22 @@ from places.models import Place
 def home(request):
     places = Place.objects.all()
     features = []
-    if places:
-        for place in places:
-            feature = {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [place.lng, place.lat]
-                },
-                "properties": {
-                    "title": place.title,
-                    "placeId": place.id,
-                    "detailsUrl": reverse("place", kwargs={"pk": place.id})
-                }
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": reverse("place", kwargs={"pk": place.id})
             }
-            features.append(feature)
+        }
+        features.append(feature)
+
     geo_data = {
       "type": "FeatureCollection",
       "features": features
