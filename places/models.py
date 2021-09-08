602300fdd343c0e58725a9a4b4f7b6f1fa65f26f
post1blues <yura1kovalyov@gmail.com>
2021-09-08 17:22:23 +0300

    Add blank=True to text fields, rename serial_number to position

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce import models as tinymce_models
 
 class Place(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
-    short_description = models.TextField(max_length=500, verbose_name="Краткое описание")
-    long_description = tinymce_models.HTMLField(null=True, blank=True, verbose_name="Полное описание")
+    short_description = models.TextField(max_length=500, verbose_name="Краткое описание", blank=True)
+    long_description = tinymce_models.HTMLField(verbose_name="Полное описание", blank=True)
     lng = models.FloatField(verbose_name="Долгота")
     lat = models.FloatField(verbose_name="Широта")
     date_created = models.DateTimeField(auto_now_add=True)
@@ -21,11 +21,11 @@ class Place(models.Model):
 
 class Image(models.Model):
     image = models.ImageField(upload_to="img", verbose_name="Изображение")
-    serial_number = models.IntegerField(default=0, verbose_name="Позиция")
+    position = models.IntegerField(default=0, verbose_name="Позиция")
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="imgs", verbose_name="Места")
 
     def __str__(self):
-        return f"{self.serial_number} {self.place.title}"
+        return f"{self.position} {self.place.title}"
 
     class Meta:
         ordering = ['serial_number']
